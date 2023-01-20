a95db01eaa4a898893e1dab2ecd6b538ec5afe15
Ayrat <ayrat.klim1@yandex.ru>
2023-01-21 02:23:34 +0300

    /

---
+++ src/js/modules/burger.js
@@ -0,0 +1,19 @@
+function burger() {
+    const burgerBtn = document.querySelector('.hamburger-lines');
+    const burgerLines = document.querySelectorAll('.line');
+    // const menu = document.querySelector('.menu');
+
+    burgerBtn.addEventListener('click', () => {
+        burgerBtn.classList.toggle('hamburger-lines_active');
+        // menu.classList.toggle('menu_active');
+
+        // Анимация кнопки
+        burgerLines.forEach((elem) => {
+            elem.classList.toggle('active');
+        });
+        
+    });
+
+}
+
+export default burger;
